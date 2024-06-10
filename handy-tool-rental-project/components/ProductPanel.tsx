e48d92eb9ae376042c2e77a8f73f4b0df358764b
tinagalido <69448352+tinagalido@users.noreply.github.com>
2024-06-10 00:20:24 -0700

    Components for Home/Login/Registration

---
+++ handy-tool-rental-project/components/ProductPanel.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const ProductPanel: React.FC = () => {
+  return (
+    <div>
+      <h3>Product Panel</h3>
+      <p>Welcome to the product!</p>
+    </div>
+  );
+};
+
+export default ProductPanel;
---
+++ handy-tool-rental-project/components/ProfilePanel.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const ProfilePanel: React.FC = () => {
+  return (
+    <div>
+      <h3>Profile Panel</h3>
+      <p>Welcome to the profile page!</p>
+    </div>
+  );
+};
+
+export default ProfilePanel;
