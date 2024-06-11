e8e98e0154a9b367d889d24efdbf87e1575780e8
tinagalido <69448352+tinagalido@users.noreply.github.com>
2024-06-11 01:14:46 -0700

    Frontend validation and UI changes

--- handy-tool-rental-project/src/components/LoginPanel.tsx
+++ handy-tool-rental-project/src/components/LoginPanel.tsx
@@ -2,79 +2,148 @@
 import React, { useState } from "react";
 import RegistrationForm from "./RegistrationForm";
 
-const LoginPanel: React.FC = () => {
+interface LoginPanelProps {
+  onLoginSuccess: () => void;
+}
+
+const LoginPanel: React.FC<LoginPanelProps> = ({ onLoginSuccess }) => {
   const [isRegistering, setIsRegistering] = useState(false);
 
   const toggleForm = () => {
     setIsRegistering(!isRegistering);
   };
 
+  /* This is for the Login validation*/
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState<string | null>(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleLogin = async (event: React.FormEvent) => {
+    event.preventDefault();
+    setIsLoading(true);
+    setError(null);
+
+    try {
+      const response = await fetch(
+        "http://ec2-52-91-173-244.compute-1.amazonaws.com:27015/api_auth/Login",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ username, password }),
+        }
+      );
+
+      const data = await response.json();
+      setIsLoading(false);
+
+      // Response available
+      if (response.status === 200) {
+        console.log("Login successful: Data" + response.body);
+        onLoginSuccess();
+      } else {
+        console.error("Login failed. Message=", response.statusText);
+        setError(response.statusText || "Login failed");
+      }
+    } catch (error) {
+      console.error("error:" + error);
+      setError("An error occurred. Please try again.");
+      setIsLoading(false);
+    }
+  };
   return (
     <div>
       {isRegistering ? (
         <RegistrationForm />
       ) : (
         <div>
-          <h3>Login Panel</h3>
-          <form className="row g-3 needs-validation">
-            <div className="row g-3 align-items-center">
+          <div className="col-md-4">
+            <h2 className="login-panel font-monospace">&nbsp;LOGIN</h2>
+          </div>
+
+          <form className="row g-3 needs-validation" onSubmit={handleLogin}>
+            <div className="row g-3 align-items-top">
               <div className="col-auto">
-                <label htmlFor="inputUsername" className="col-form-label">
-                  Username
+                <label htmlFor="username" className="col-form-label">
+                  <p className="fw-bold">&nbsp;&nbsp;&nbsp;USERNAME</p>
                 </label>
               </div>
               <div className="col-auto">
                 <input
                   type="text"
                   className="form-control"
-                  id="inputUsername"
+                  id="username"
+                  name="username"
                   placeholder="Username"
                   aria-label="Username"
+                  autoComplete="off"
                   required
+                  onChange={(e) => setUsername(e.target.value)}
                 />
               </div>
               <div className="col-auto">
                 <span id="passwordHelpInline" className="form-text">
-                  Format: email@test.com
+                  <p className="fw-semibold">Format: email@test.com</p>
                 </span>
               </div>
             </div>
-            <div className="row g-3 align-items-center">
+            <div className="row g-3 align-items-top">
               <div className="col-auto">
-                <label htmlFor="inputPassword" className="col-form-label">
-                  Password
+                <label htmlFor="password" className="col-form-label">
+                  <p className="fw-bold">&nbsp;&nbsp;&nbsp;PASSWORD</p>
                 </label>
               </div>
               <div className="col-auto">
                 <input
                   type="password"
                   className="form-control"
-                  id="userPassword"
+                  id="password"
+                  name="password"
+                  autoComplete="off"
                   placeholder="Password"
                   aria-label="Password"
                   aria-describedby="passwordHelpInline"
                   required
+                  onChange={(e) => setPassword(e.target.value)}
                 />
               </div>
               <div className="col-auto">
                 <span id="passwordHelpInline" className="form-text">
-                  Must be 8-20 characters long.
+                  <p className="fw-semibold">Must be 3-10 characters long</p>
                 </span>
               </div>
             </div>
 
-            <div className="col-12">
-              <button className="btn btn-primary" type="submit">
-                Login
-              </button>
+            <div className="row g-3 align-items-top">
+              <div className="col-auto">
+                <p></p>
+              </div>
+              <div className="col-auto">
+                <p className="fw-semibold">Already a member?</p>
+              </div>
+              <div className="col-auto">
+                <button
+                  className="btn btn-primary"
+                  type="submit"
+                  disabled={isLoading}
+                >
+                  {isLoading ? "Logging in..." : "Login"}
+                </button>
+                {error && <div className="error">{error}</div>}
+              </div>
             </div>
 
-            <div className="d-flex mb-3">
-              <div className="p-2">
+            <div className="row g-3 align-items-top">
+              <div className="col-auto">
+                <p></p>
+              </div>
+              <div className="col-auto">
                 <p className="fw-semibold">Not yet registered?</p>
               </div>
 
-              <div className="p-2">
+              <div className="col-auto">
                 <button
                   type="button"
                   className="btn btn-success"
@@ -83,7 +152,6 @@ const LoginPanel: React.FC = () => {
                   Register
                 </button>
               </div>
-              <div className="p-2"></div>
             </div>
           </form>
         </div>
