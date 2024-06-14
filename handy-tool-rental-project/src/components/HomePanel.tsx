39b10c6611cd577e34ee76f55fc8d6b43a26db83
tinagalido <69448352+tinagalido@users.noreply.github.com>
2024-06-13 18:20:38 -0700

    Updated UI layout and components

--- handy-tool-rental-project/src/components/HomePanel.tsx
+++ handy-tool-rental-project/src/components/HomePanel.tsx
@@ -3,8 +3,21 @@ import React from "react";
 const HomePanel: React.FC = () => {
   return (
     <div>
-      <h3>Home Panel</h3>
-      <p>Welcome to the home page!</p>
+      <div className="col-md-12">
+        {/* Text Section */}
+        <div className="text-section container my-5">
+          <h2 className="text-center">Welcome to Our Website</h2>
+          <p className="text-center">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
