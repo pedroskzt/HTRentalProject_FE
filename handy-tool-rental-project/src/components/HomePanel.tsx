fc2447f458ad6d66534cec74109b5442c0b24d08
ghazalshammasi <ghazal.shammasi@gmail.com>
2024-07-18 18:28:49 -0700

    included chatbot in the home panel

--- handy-tool-rental-project/src/components/HomePanel.tsx
+++ handy-tool-rental-project/src/components/HomePanel.tsx
@@ -1,7 +1,44 @@
 import React from "react";
+import { useState } from "react";
 import "../HomePanel.css";
 
+// insert function URL for openAI here:
+const functionUrl = "https://xxxxxxxxxxxxxxxx.lambda-url.eu-west-3.on.aws/";
+
+type Message = {
+  text: string;
+  sender: "ai" | "user";
+};
+
 const HomePanel: React.FC = () => {
+  // creating chat bot
+  const [newInputValue, setNewInputValue] = useState("");
+  const [messages, setMessages] = useState<Message[]>([]);
+
+  const newMessage: React.FormEventHandler = async (e) => {
+    e.preventDefault();
+    setNewInputValue("");
+    const newMessages: Message[] = [
+      ...messages,
+      {
+        text: newInputValue,
+        sender: "user",
+      },
+    ];
+    setMessages(newMessages);
+    const response = await fetch(functionUrl, {
+      method: "POST",
+      body: JSON.stringify({ messages: newMessages }),
+    });
+    setMessages([
+      ...newMessages,
+      {
+        sender: "ai",
+        text: await response.text(),
+      },
+    ]);
+  };
+
   return (
     <div className="home-panel">
       <div className="left-section">
@@ -23,6 +60,23 @@ const HomePanel: React.FC = () => {
             perfect one for your needs.
           </p>
         </div>
+        <h1>Need Help?</h1>
+        <div>
+          {messages.map((message, index) => (
+            <p key={index} className={"message " + message.sender}>
+              {message.text}
+            </p>
+          ))}
+        </div>
+        <form className="input-form" onSubmit={newMessage}>
+          <input
+            type="text"
+            placeholder="Message"
+            value={newInputValue}
+            onChange={(e) => setNewInputValue(e.currentTarget.value)}
+          />
+          <input type="submit" value="Send" />
+        </form>
       </div>
     </div>
   );
